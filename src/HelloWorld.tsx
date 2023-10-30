9d7b91dea7dafa824758d79966105e0274d4e330
Arnaud Laforgue <arnaud.laforgue@gmail.com>
2023-10-30 11:06:07 +0100

    first commit

---
+++ src/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloWorld = () => {
+    return (
+        <div>
+            <h1 style={{color:"red"}}>HelloWorld</h1>
+        </div>
+    );
+};
+
+export default HelloWorld;
